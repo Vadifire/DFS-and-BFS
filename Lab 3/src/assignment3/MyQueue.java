16e31dbffca911a0aae22b53834de37aabe2bf01
giladcroll <giladcroll@utexas.edu>
2016-09-28 15:07:23 -0500

    added ladder field to Node and functions to fetch it

--- Lab 3/src/assignment3/MyQueue.java
+++ Lab 3/src/assignment3/MyQueue.java
@@ -1,22 +1,26 @@
 package assignment3;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class MyQueue {
-	private static class QueueNode<T>{
+	public static class QueueNode<String>{
 		private String data;
 		private QueueNode<String> next;
-		
-		public QueueNode(String data) {
+		private QueueNode<String> father;
+		private ArrayList<String> ladder;
+		public QueueNode(String data, ArrayList<String> dadLadder) {
 			this.data = data;
+			this.ladder = (ArrayList<String>) dadLadder.clone();
+			this.ladder.add(data);
 		}
 	}
 	
 	private QueueNode<String> first;
 	private QueueNode<String> last;
 	
-	public void add(String str){
-		QueueNode<String> node = new QueueNode<String> (str);
+	public void add(String str, ArrayList<String> dadLadder){
+		QueueNode<String> node = new QueueNode<String>(str, dadLadder);
 		if (last !=null)
 			last.next = node;
 		last = node;
@@ -24,18 +28,25 @@ public class MyQueue {
 			first = last;
 	}
 	
-	public String remove(){
+	
+
+	
+	public QueueNode<java.lang.String> remove(){
 		if (first == null) throw new NoSuchElementException();
-		String data = first.data;
 		first = first.next;
 		if (first == null)
 			last = null;
-		return data;
+		return first;
 	}
 	
 	public String peek(){
 		if (first == null) throw new NoSuchElementException();
 		return first.data;
+	}
+	
+	public ArrayList<java.lang.String> peekLadder(){
+		if (first == null) throw new NoSuchElementException();
+		return first.ladder;
 	}
 	
 	public boolean isEmpty(){
